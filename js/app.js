15f9a3ee35fd017e9ec17bada9550edd5e03a467
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-17 17:13:50 -0600

    data types

---
+++ js/app.js
@@ -0,0 +1,50 @@
+// console.log("test");
+
+// Q1)
+
+// Boolean
+lightSwitchIsOn = true;
+    
+// String
+let userEmailAddress = "td@gmail.com";
+    
+// Object    
+const spaceship = {
+    weapons: ["laser blasters", "tractor beam"],
+    engine: "warp drive",
+    storage: "hull"
+ };
+    
+// Array
+let stundents = ["name1", "name2", "name3"];
+
+//Object
+const students = {
+    name1: {
+        location: "local1",
+    },
+    name2: {
+        location: "local2",
+    },
+    name3: {
+        location: "local3"
+    }
+};
+
+//Object
+
+const students2 = {
+    name1: {
+        location: "local1",
+        favoriteTvShows: ["tvShow1", "tvShow2"],
+    },
+    name2: {
+        location: "local2",
+        favoriteTvShows: ["tvShow1", "tvShow2"],
+    },
+    name3: {
+        location: "local3",
+        favoriteTvShows: ["tvShow1", "tvShow2"],
+    }
+};
+
