44da8d120ae849aba4955ec84b45fa3c16349e9e
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-17 17:36:02 -0600

    object-ception

--- js/app.js
+++ js/app.js
@@ -108,4 +108,27 @@ const crazyObject = {
   console.log(crazyObject.larry.characters[2].favourtieHobby);
   console.log(crazyObject.larry.nicknames[1]);
   console.log(crazyObject.larry.characters[1]);
-  
+
+// Section 4. Object-ception
+
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+inception.reality.limbo = null;
+console.log(inception.reality.limbo);
+
